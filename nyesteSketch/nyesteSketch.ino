8ab3a7e15d67eaaa0577006adc6c09033ba51860
FriendlyFisting <43848000+FriendlyFisting@users.noreply.github.com>
2020-05-24 23:02:45 +0200

    audrino code

---
+++ nyesteSketch/nyesteSketch.ino
@@ -0,0 +1,45 @@
+#include <NewPing.h>
+
+#define SONAR_NUM     3 // Number or sensors.
+#define MAX_DISTANCE 20 // Max distance in cm.
+#define PING_INTERVAL 200 // Milliseconds between pings.
+ 
+unsigned long pingTimer[SONAR_NUM]; // When each pings.
+unsigned int cm[SONAR_NUM]; // Store ping distances.
+uint8_t currentSensor = 0; // Which sensor is active.
+ 
+NewPing sonar[SONAR_NUM] = { // Sensor object array.
+  NewPing(2, 3, MAX_DISTANCE),
+  NewPing(9, 8, MAX_DISTANCE),
+  NewPing(11, 10, MAX_DISTANCE),
+};
+ 
+void setup() {
+  Serial.begin(9600);
+  pingTimer[0] = millis() + 75; // First ping start in ms.
+  for (uint8_t i = 1; i < SONAR_NUM; i++)
+    pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;
+}
+ 
+void loop() {
+  for (uint8_t i = 0; i < SONAR_NUM; i++) {
+    if (millis() >= pingTimer[i]) {
+      pingTimer[i] += PING_INTERVAL * SONAR_NUM;
+      if (i == 0 && currentSensor == SONAR_NUM - 1)
+        oneSensorCycle(); // Do something with results.
+      sonar[currentSensor].timer_stop();
+      currentSensor = i;
+      cm[currentSensor] = 0;
+      sonar[currentSensor].ping_timer(echoCheck);
+    }}}
+ 
+void echoCheck() { // If ping echo, set distance to array.
+  if (sonar[currentSensor].check_timer())
+    cm[currentSensor] = sonar[currentSensor].ping_result / US_ROUNDTRIP_CM;
+}
+ 
+void oneSensorCycle() { // Do something with the results.
+  for (uint8_t i = 0; i < SONAR_NUM; i++) {
+    Serial.write((i*100)+(cm[i]/10));
+  }
+}
